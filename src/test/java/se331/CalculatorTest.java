b2992c6c335fe891d4c47066aaeb4eeaca9cf712
kengchayodom <chayodom_j@gmail.com>
2025-09-29 13:49:04 +0700

    fix more coverage

--- src/test/java/se331/CalculatorTest.java
+++ src/test/java/se331/CalculatorTest.java
@@ -1,32 +1,32 @@
-
+package se331;
 
 import org.junit.jupiter.api.Test;
 import se321.Calculator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CalculatorTest {
+ class CalculatorTest {
 
     @Test
-    public void testAdd() {
+     void testAdd() {
         Calculator calculator = new Calculator();
         assertEquals(5, calculator.add(2, 3));
     }
 
     @Test
-    public void testSubtract() {
+     void testSubtract() {
         Calculator calculator = new Calculator();
         assertEquals(1, calculator.subtract(5, 4));
     }
 
     @Test
-    public void testMultiply() {
+     void testMultiply() {
         Calculator calculator = new Calculator();
         assertEquals(6, calculator.multiply(2, 3));
     }
 
     @Test
-    public void testDivide() {
+     void testDivide() {
         Calculator calculator = new Calculator();
         // The last argument '0.01' is the delta for comparing double-precision numbers
         assertEquals(2.0, calculator.divide(4, 2), 0.01);
